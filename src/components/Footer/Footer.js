0f8b08e29c41867d3cf184f5afb653109a55d571
Liudmyla <milat6707@gmail.com>
2022-03-05 10:41:05 +0200

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import './Footer.css'
+
+export const Footer = ({year}) => {
+  return (
+    <footer>
+      <span>© Chocoladochka Blog - {year}</span>
+    </footer>
+  );
+}
---
+++ src/shared/projectData.js
@@ -0,0 +1,23 @@
+export const posts = [
+  {
+    id: 1,
+    title: "Post 1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum blanditiis deserunt, harum dolores provident reiciendis voluptatem rem nam exercitationem hic magni modi? Ut sed deserunt iusto ex porro voluptate officia!",
+    liked: false,
+  },
+  {
+    id: 2,
+    title: "Post 2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum blanditiis deserunt, harum dolores provident reiciendis voluptatem rem nam exercitationem hic magni modi? Ut sed deserunt iusto ex porro voluptate officia!",
+    liked: false,
+  },
+  {
+    id: 3,
+    title: "Post 3",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum blanditiis deserunt, harum dolores provident reiciendis voluptatem rem nam exercitationem hic magni modi? Ut sed deserunt iusto ex porro voluptate officia!",
+    liked: false,
+  },
+];
